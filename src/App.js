0512de89d7f2e480515b584f92149ea168b7ba98
Toni Vänttinen <Toni.vanttinen@dnainternet.net>
2020-01-31 15:21:13 +0200

    FetchData
    Add: fetch data from backend

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import './App.scss';
 import Header from "./HeaderApp.js";
 import Citie from "./CitieInfo.js";
 import Dropdown from "./DropdownMenu.js"
+import fetchData from './fetchData';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.data = new fetchData();
+    this.data.getWeatherFrom('634964')
+  }
 
   render() {
     return (
