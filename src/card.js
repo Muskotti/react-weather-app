9e5c92f2539a39627dd9cf8bef14c222e2163710
Toni Vänttinen <Toni.vanttinen@dnainternet.net>
2020-01-31 11:27:02 +0200

    Font, header, card
    Add: Correct font, Header for the app, and base card styling

---
+++ src/card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="card">
+      <div className="container">
+        <p>citie info</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Header">
+      <p>Säätutka</p>
+    </div>
+  );
+}
+
+export default App;
